37b4ccfada1dc6332f3bd6cd52907f36bb69019d
Dukuk1 <100125031+Dukuk1@users.noreply.github.com>
2025-02-17 13:06:39 +0300

    Update node.js

--- node.js
+++ node.js
@@ -1,15 +1,22 @@
-var button = document.getElementById("rnd_btn")
 
-const code_verifier = "bc2b3b9656a9308f45e0982a6a4272907b56f3927d01db80f0e06dea"
-const code_challenge = "3qR6vaeerzuZoa6_hzrAas7bHS9nyXXtO2gE1B6jx2Y"
+async function getRandomAnime() {
+    var respon = await fetch(`https://api.jikan.moe/v4/random/anime`)
+    var data = await respon.json()
 
-var URL = `https://myanimelist.net/v1/oauth2/authorize?
-response_type=code
-&client_id=${2}
-&state=${1}
-&code_challenge=${code_challenge}
-&code_challenge_method=plain`;
+    return data
+}
 
-button.addEventListener("click",()=>{
+
+var img = document.getElementById("img_1")
+
+var button = document.getElementById("btn")
+
+button.addEventListener("click", () => {
+    getRandomAnime().then(data => {
+        img.src = data.data.images.jpg.image_url;
+        console.log(data)
+    })
 
 })
+
+
